558c67959796ae0ebba2b387fab789dc3d8e03f2
Hunain2003 <65592359+Hunain2003@users.noreply.github.com>
2023-09-02 07:55:51 +0000

    basic script for timer

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,7 +1,25 @@
-function startTimer() {
+let startBtn = document.querySelector('#start-btn');
+let stopBtn = document.querySelector('#stop-btn');
 
+function startTimer() {
+    let interval = document.querySelector('#time').value;
+    if (!interval) {
+        alert('Error no value');
+        return;
+    }
+    startBtn.disabled = true;
+    setInterval(alert('Timer completed.') , interval);
 }
 
 function stopTimer() {
-
+    clearInterval(startTimer)
+    startBtn.removeAttribute('disabled');
 }
+
+startBtn.addEventListener('click', e => {
+    startTimer();
+});
+
+stopBtn.addEventListener('click', e => {
+    stopTimer();
+})
